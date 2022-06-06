fb9d3a3ff50c9da24e3dfe1fcbe6519ee3fdfa6c
MargaretGoncharenko <margaret.goncharenko@gmail.com>
2022-06-06 16:19:12 +0300

    Task10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import loader from "./../../loader.gif"
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
-
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
     };
 
     return (
         <div>
-            <hr/>
-            homeworks 10
-
-            {/*should work (должно работать)*/}
-            {loading
+            <a>homeworks 10</a>
+            {isLoading
                 ? (
-                    <div>крутилка...</div>
+                    <div><img src={loader}/></div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
             }
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
         </div>
     )
 }
 
 export default HW10
+
+
+{/*для личного творчества, могу проверить*/
+}
+{/*<Alternative/>*/
+}
